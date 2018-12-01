a3f89564ae9d1b3417a59a70195f24bb0072ff36
cecois <cecmcgee@gmail.com>
2018-12-01 16:14:47 -0500

    env and z

--- atl-send.js
+++ atl-send.js
@@ -1,11 +1,13 @@
+require('dotenv').load();
+
 const __ = require('underscore')
 ,AXIOS = require('axios')
 ,MOMENT = require('moment')
 ,MONGO = require('mongodb').MongoClient
-,TWEET = require('tweet-tweet')
+,TWITTER = require('twitter')
 ,TINY = require('tinyurl')
 ,PUPPETEER = require('puppeteer')
-,CONFIG = require("./Config.json")
+// ,CONFIG = require("./Config.json")
 // ,CLOUDINARY = require('cloudinary')
 // ,CLOUD = require("cloudinary-direct")
 ,FS = require('fs')
@@ -38,12 +40,19 @@ const __ = require('underscore')
 ]
 ;
 
+const TWITR = new TWITTER({
+  consumer_key: process.env.TW_API_KEY,
+  consumer_secret: process.env.TW_API_SECRET,
+  access_token_key: '',
+  access_token_secret: ''
+});
+
 const _SET_NEW_LOW=(nid)=>{
  return new Promise(function(resolve, reject) {
 
   var Q = { "id":nid }
 
-  const uri = "mongodb+srv://app:7GT8Cdl*fq4Z@cl00-uacod.mongodb.net/parking?retryWrites=true";
+  const uri = "mongodb+srv://app:"+process.env.MONGOPSSWD+"@cl00-uacod.mongodb.net/parking?retryWrites=true";
   MONGO.connect(uri, { useNewUrlParser: true }, function(err, client) {
    const col = client.db("parking").collection("atl_parcel_parking");
    // perform actions on the collection object
@@ -60,6 +69,7 @@ const _SET_NEW_LOW=(nid)=>{
 }
 
 const _GET_LOW=()=>{
+  console.log("fetching lowest id...")
  return new Promise(function(resolve, reject) {
 
   var Q = {"sent":{$eq:null}}
@@ -78,6 +88,16 @@ const _GET_LOW=()=>{
 
 	});//Promise
 }
+
+const _GETBIO = async(I)=>{
+
+console.log("fetching bio for ",I)
+   let bio = await _GET('https://cecmcgee.carto.com/api/v2/sql?',{params:{q:"SELECT bio_short FROM atl_tax_parcel_parking WHERE cartodb_id="+I}})
+  return new Promise((resolve,reject)=>{
+if(!bio){reject("no bio found at carto")}
+   resolve(__.first(bio.data.rows).bio_short);
+})//promise
+}//_GET
 
 const _GET = async(U,P)=>{
 
@@ -106,7 +126,7 @@ const _TINIFY = async (U) => {
 
 const _CAPIFY = async(U,F)=>{
 
-  console.log("screencapping...",U)
+  // console.log("screencapping...",U)
   let fi = F
 
   const browser = await PUPPETEER.launch({slowMo: 500});
@@ -120,52 +140,84 @@ const _CAPIFY = async(U,F)=>{
 
 }//capify
 
-const _TWEET = async(U,IMG)=>{
+const _TWEET = async(T,IMG)=>{
 	return new Promise((resolve,reject)=>{
 
-//resolve result of tweet package
+// Load your image
+var img = FS.readFileSync(IMG);
+
+// Make post request on media endpoint. Pass file data as media parameter
+TWITR.post('media/upload', {media: img}, function(error, media, response) {
+if(error){console.log(error);reject(error)}
+  if (!error) {
+
+    // If successful, a media object will be returned.
+    console.log(media);
+
+    // Lets tweet it
+    var status = {
+      status: T,
+      media_ids: media.media_id_string // Pass the media id string
+    }
+
+    TWITR.post('statuses/update', status, function(error, tweet, response) {
+if(error){console.log(error);reject(error)}
+      if (!error) {
+        console.log(tweet);
+        resolve(tweet);
+      }
+    });
+
+  }
+});
 
 	})//promise
 }//tweet
 
 const _GETMAP = async()=>{
-	return new Promise((resolve,reject)=>{
+  console.log("fetching random map")
+  return new Promise((resolve,reject)=>{
 
     resolve(maps[Math.floor(Math.random()*maps.length)])
 
-	})//promise
+  })//promise
 }//tweet
 
 const main = async () =>{
 
-//get lowest id where carto.sent IS NULL
-// let lows = await _GET('https://cecmcgee.carto.com/api/v2/sql?',{params:{q:"SELECT min(objectid) as low FROM atl_tax_parcel_parking WHERE sent IS NULL"}})
+//get lowest id frm mongo
 let lowob = await _GET_LOW()
+// console.log("lowob (full)",lowob);
 let low=lowob[0].id
-if(typeof low == 'undefined'){console.log("no low id found")}
+if(typeof low == 'undefined'){console.log("no low id found")}else{console.log("lowest id is ",low)}
 
-	let map = await _GETMAP();
+let map = await _GETMAP();
+console.log("random map is ",map)
+let bio = await _GETBIO(low);
+console.log("bio is ",map)
+// console.log("bio",bio);
 
 let uri = "http://lots.milleria.org/#/"+map+"/-84.86732482910156,33.608900856100234,-83.83323669433595,33.897777013859475/"+low
-console.log("cap",uri);
 
 let capfile = '/tmp/'+low+".png"
 
-// optionally pull screencap
+// pull screencap
 let cap = await _CAPIFY(uri,capfile)
-console.log("capfile",capfile)
 
 // place it
 // let capurl = await _UPCAP(capfile)
 // console.log("capurl",capurl);
 
-// gen tiny urik
-// let url = await _TINIFY(uri)
+// gen tiny uri
+let url = await _TINIFY(uri)
 // console.log("uri",url);
 
+let tweet = bio+" "+url
+console.log("tweeting: ",tweet);
 
 //tweet short bio
-// let tweet = await _TWEET(uri,cap);
+let tweeted = await _TWEET(tweet,capfile);
+console.log("tweeted",tweeted);
 //update carto set sent=true (or timestamp maybe)
 // let finalize = await _GET('https://cecmcgee.carto.com/api/v2/sql?',{params:{q:"update atl_tax_parcel_parking set sent="}})
 
